e153100c2c771b02130339d864dc7e533145cace
Daniel Gallups <dsgallups@protonmail.com>
2025-05-11 10:33:39 -0400

    init plugin and scaffold exampel

--- examples/human.rs
+++ examples/human.rs
@@ -1,4 +1,26 @@
 use bevy::prelude::*;
-use bevy_gltf_animation::prelude::*;
+use bevy_gltf_animation::GltfAnimationPlugin;
 
-fn main() {}
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, GltfAnimationPlugin))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+#[derive(Component)]
+struct Human;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.0, 3.0, 3.0).looking_at(Vec3::Y * 0.5, Vec3::Y),
+    ));
+    commands.spawn((
+        DirectionalLight::default(),
+        Transform::IDENTITY.looking_to(Vec3::new(-0.5, -0.5, -1.0), Vec3::Y),
+    ));
+
+    //human
+    commands.spawn((Human, SceneRoot(asset_server.load("human.glb#Scene0"))));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-pub mod prelude {}
+use bevy::prelude::*;
+
+pub struct GltfAnimationPlugin;
+
+impl Plugin for GltfAnimationPlugin {
+    fn build(&self, app: &mut App) {
+        //todo
+    }
+}
+
+pub mod prelude {
+    pub use crate::GltfAnimationPlugin;
+}
